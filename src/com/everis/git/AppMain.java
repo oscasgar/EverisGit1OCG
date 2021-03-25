7a7d3459a3b9105321b432b8f1b1567b08f4c48c
ocastrog <ocastrog@everis.com>
2021-03-25 11:42:06 +0100

    version 1 - desafio GIT

---
+++ src/com/everis/git/AppMain.java
@@ -0,0 +1,21 @@
+package com.everis.git;
+
+public class AppMain {
+
+	/**
+	 * Principal de prueba de GIT
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/**
+		 * Impresion en pantalla de el String "Hola mundo"
+		 */
+		System.out.println("Hola mundo");
+		
+		
+	}
+
+}
